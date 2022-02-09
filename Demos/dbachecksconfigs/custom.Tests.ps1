4adef68df767ab6ab742752694d3ff1ece352781
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-02-09 18:05:02 +0000

    so we dont have databases on the 1 that are not needed

--- Demos/dbachecksconfigs/custom.Tests.ps1
+++ Demos/dbachecksconfigs/custom.Tests.ps1
@@ -1,5 +1,58 @@
 $filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
-Describe "dbatools2 should not have the databases already" -Tags NoDatabases , $Filename {
+Describe "dbatools2 should not have the databases already" -Tags NoDatabasesOn2 , $Filename {
+    Context  "Databases Should not exist" {
+        BeforeAll {
+            $Databasesondbatools2 = (Get-DbaDatabase -SqlInstance $dbatools2).Name
+        }
+        $TestCases = @(
+            @{
+                Name = 'pubs-0'
+            },
+            @{
+                Name = 'pubs-1'
+            },
+            @{
+                Name = 'pubs-10'
+            },
+            @{
+                Name = 'pubs-2'
+            },
+            @{
+                Name = 'pubs-3'
+            },
+            @{
+                Name = 'pubs-4'
+            },
+            @{
+                Name = 'pubs-5'
+            },
+            @{
+                Name = 'pubs-6'
+            },
+            @{
+                Name = 'pubs-7'
+            },
+            @{
+                Name = 'pubs-8'
+            },
+            @{
+                Name = 'pubs-9'
+            },
+            @{
+                Name = 'pubs'
+            },
+            @{
+                Name = 'Northwind'
+            }
+        )
+        It "<Name> Database should not exist on dbatools2" -TestCases $TestCases {
+            Param($Name)
+            $Name | Should -Not -BeIn $Databasesondbatools2 -Because "It will make demos pretty tricky"
+        }
+    }
+}
+$filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Describe "dbatools1 should not have the additional databases already" -Tags NoDatabasesOn1 , $Filename {
     Context  "Databases Should not exist" {
         BeforeAll {
             $Databasesondbatools2 = (Get-DbaDatabase -SqlInstance $dbatools2).Name
