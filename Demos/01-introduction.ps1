7b49214d16c695638ca16dd82c637c69420b20ea
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-02-22 09:26:12 +0000

    cos claudio is ace

--- Demos/01-introduction.ps1
+++ Demos/01-introduction.ps1
@@ -43,58 +43,36 @@ Find-DbaCommand -Pattern role | Out-GridView -PassThru | Get-Help -Full
 # or we use Microsoft.PowerShell.ConsoleGuiTools for this in Powershell 7
 Get-Help (Find-DbaCommand -Pattern role |Select CommandName, Synopsis | Out-ConsoleGridView).CommandName 
 
+# Lets take a look at some things
 
+# Databases
 
+Get-DbaDatabase -SqlInstance $dbatools1
 
+Get-DbaDatabase -SqlInstance $dbatools1 | Select ComputerName,InstanceName,SqlInstance,Name,Status,db_owner
 
+# Logins
+Get-DbaLogin -SqlInstance $dbatools1
 
-## Lets look at the linked servers on sql0
+Get-DbaLogin -SqlInstance $dbatools1 |Format-Table
 
-Get-DbaLinkedServer -SqlInstance $SQLInstances[0] | Format-Table
+# We can put multiple instances in any SqlInstance parameter
 
-## I wonder if they are all working correctly
+Get-DbaLogin -SqlInstance $dbatools1, $dbatools2 |Format-Table
 
-Test-DbaLinkedServerConnection -SqlInstance $sql0 
+# We can pipe commands together
 
-## Lets have a look at the linked servers on sql1
+Get-DbaDatabase -SqlInstance $dbatools1,$dbatools2 -ExcludeSystem | Get-DbaDbFile 
 
-Get-DbaLinkedServer -SqlInstance $sql1
+# What else would you like to look at on an instance ?
 
-## Ah - There is an Availability Group here
-## I probably want to make sure that each instance has the same linked servers
-## but they have sql auth and passwords - where are the passwords kept ?
-
-(Get-DbaLinkedServer -sqlinstance $sql0)[0] | Select-Object SQLInstance, Name, RemoteServer, RemoteUser
-
-## I can script out the T-SQL for the linked server
-(Get-DbaLinkedServer -sqlinstance $sql0)[0] | Export-DbaScript 
-
-## But I cant use the password :-(
-Get-ChildItem *sql0-LinkedServer-Export* | Open-EditorFile
-
-## Its ok, with dbatools I can just copy them over anyway :-) Dont need to know the password
-
-Copy-DbaLinkedServer -Source $sql0 -Destination $sql1
-
-## Now lets look at sql1 linked servers again
-
-Get-DbaLinkedServer -SqlInstance $sql1 | Format-Table
-
-## Lets test them to show we have the Password passed over as well
-
-Test-DbaLinkedServerConnection -SqlInstance $sql1
-
-#endregion
-
-#region Look at Builds
+# 
 
 $builds = Get-DbaBuildReference -SqlInstance $SQLInstances 
 
 $Builds | Format-Table
 
 Get-DbaBuildReference -Build 10.0.6000,10.50.6000 |Format-Table
-
-#endregion
 
 # Choose your adventure
 
