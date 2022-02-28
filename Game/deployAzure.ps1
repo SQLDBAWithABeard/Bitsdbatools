9ed72dd59d98aae25bc4a6a6e0a05f1a5ad13f26
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-02-28 19:31:01 +0000

    make it pretty

--- Game/deployAzure.ps1
+++ Game/deployAzure.ps1
@@ -2,35 +2,35 @@ $ShowAzure = $true
 Connect-AzAccount
 Set-AzContext -Subscription Pay-As-You-Go
 
-$securePassword = ('dbatools.IO' | ConvertTo-SecureString -asPlainText -Force)
+$securePassword = ('dbatools.IO' | ConvertTo-SecureString -AsPlainText -Force)
 $continercredential = New-Object System.Management.Automation.PSCredential('sqladmin', $securePassword)
 
 # Validate the deployment with Whatif
 $DeploymentConfig = @{
-    TemplateFile      = 'Game\demoazure.bicep'
-    location          = 'westeurope'
-    locationFromTemplate          = 'westeurope'
-    rgName = 'SqlBitsGame'
-    nameFromTemplate = 'TheHauntedHouse'
-    dbName = 'Chapter22'
-    administratorLogin = $continercredential.UserName
+    TemplateFile               = 'Game\demoazure.bicep'
+    location                   = 'westeurope'
+    locationFromTemplate       = 'westeurope'
+    rgName                     = 'SqlBitsGame'
+    nameFromTemplate           = 'TheHauntedHouse'
+    dbName                     = 'Chapter22'
+    administratorLogin         = $continercredential.UserName
     administratorLoginPassword = $continercredential.Password
-    WhatIf            = $true
+    WhatIf                     = $true
 }
 New-AzDeployment @DeploymentConfig
 
 # deploy
 $DeploymentConfig = @{
-    TemplateFile      = 'Game\demoazure.bicep'
-    location          = 'westeurope'
-    locationFromTemplate          = 'westeurope'
-    rgName = 'SqlBitsGame'
-    nameFromTemplate = 'TheHauntedHouse'
-    dbName = 'Chapter22'
-    administratorLogin = $continercredential.UserName
+    TemplateFile               = 'Game\demoazure.bicep'
+    location                   = 'westeurope'
+    locationFromTemplate       = 'westeurope'
+    rgName                     = 'SqlBitsGame'
+    nameFromTemplate           = 'TheHauntedHouse'
+    dbName                     = 'Chapter22'
+    administratorLogin         = $continercredential.UserName
     administratorLoginPassword = $continercredential.Password
-    AADLogin = 'Beard_SQLAdmins'
-    AADSid = 'f27e046a-0b1e-4c1c-a1da-ccff56f63148'
-    AADType = 'Group'
+    AADLogin                   = 'Beard_SQLAdmins'
+    AADSid                     = 'f27e046a-0b1e-4c1c-a1da-ccff56f63148'
+    AADType                    = 'Group'
 }
 New-AzDeployment @DeploymentConfig
