8e7fb25d039871f908e938374eae78e531f3802a
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-03-03 13:20:32 +0000

    so things are checked

--- Demos/12-EstateValidation.ps1
+++ Demos/12-EstateValidation.ps1
@@ -1,9 +1,21 @@
+<#
+_____    _        _         _   _       _ _     _       _   _             
+|  ___|  | |      | |       | | | |     | (_)   | |     | | (_)            
+| |__ ___| |_ __ _| |_ ___  | | | | __ _| |_  __| | __ _| |_ _  ___  _ __  
+|  __/ __| __/ _` | __/ _ \ | | | |/ _` | | |/ _` |/ _` | __| |/ _ \| '_ \ 
+| |__\__ \ || (_| | ||  __/ \ \_/ / (_| | | | (_| | (_| | |_| | (_) | | | |
+\____/___/\__\__,_|\__\___|  \___/ \__,_|_|_|\__,_|\__,_|\__|_|\___/|_| |_|
+#>
+
+# How do we know our estate is in the perfect state?
+
 # dbachecks is the sister module to dbatools
-# It is Rob's baby and uses the PowerShell module Pester
-# to validate the estate
+# It is Rob's baby and uses the PowerShell module Pester to validate the estate
 # we have been using it all day, you just haven't seen it!
 
 # You install it in the same way as any PowerShell module
+
+# Install-Module dbachecks
 
 # You have a number of checks. These are unique - 
 # Yes or No, True or False, Passed or Failed
@@ -11,7 +23,17 @@
 
 Get-DbcCheck
 
-Get-DbcCheck | ocgv
+# That was a lot - let's just look at the Database group
+Get-DbcCheck -Group Database
+
+# Find any checks that mention backup
+Get-DbcCheck -Pattern *Backup*
+
+# Can find any checks with the Backup tag
+Get-DbcCheck -Tag Backup
+
+# Also works for unique tags
+Get-DbcCheck -Tag LastFullBackup
 
 # The checks are grouped by Agent,Database,Domain,HADR,Instance, LogShipping,MaintenanceSolution and Server
 
@@ -80,84 +102,73 @@ Invoke-DbcCheck -SqlInstance $dbatools1,$dbatools2 -Check LastGoodCheckDb
 
 # but you can also use it to validate that your settings are correct
 
-# FOr Example - You might choose to ensure that auto-close is always set on to save on (well I dont know hwy you would but ..)
 
-Invoke-DbcCheck -SqlInstance $dbatools1,$dbatools2 -Check auto-close
-
-# So we have default config values that we think make sense
+# We have default config values that we think make sense
 # or are recommended by people you know and trust and whose blogs you read
 # but everything is configurable
 
+# Lets configure a check
 
-<#
-_____    _        _         _   _       _ _     _       _   _             
-|  ___|  | |      | |       | | | |     | (_)   | |     | | (_)            
-| |__ ___| |_ __ _| |_ ___  | | | | __ _| |_  __| | __ _| |_ _  ___  _ __  
-|  __/ __| __/ _` | __/ _ \ | | | |/ _` | | |/ _` |/ _` | __| |/ _ \| '_ \ 
-| |__\__ \ || (_| | ||  __/ \ \_/ / (_| | | | (_| | (_| | |_| | (_) | | | |
-\____/___/\__\__,_|\__\___|  \___/ \__,_|_|_|\__,_|\__,_|\__|_|\___/|_| |_|
-#>
-
-# How do we know our estate is in the perfect state?
-
-# dbachecks!
-Import-Module dbachecks
-
-# What can we check
-Get-DbcCheck 
-
-# That was a lot - let's just look at the Database group
-Get-DbcCheck -Group Database
-
-# Find any checks that mention backup
-Get-DbcCheck -Pattern *Backup*
-
-# Can find any checks with the Backup tag
-Get-DbcCheck -Tag Backup
-
-# Also works for unique tags
-Get-DbcCheck -Tag LastFullBackup
-
-# Run some checks - are we doing backups
-Invoke-DbcCheck -SqlInstance $SQLInstances -Check LastFullBackup
-
-# Do we have AutoClose turned off - and is the SA account disabled
-    # combining instance level & database level checks
+#Run some checks!
 Invoke-DbcCheck -SqlInstance $SQLInstances -Check AutoClose, SaDisabled
 
-# But only show us what we care about
-Invoke-DbcCheck -SqlInstance $SQLInstances -Check AutoClose, SaDisabled -Show Fails
-
-# Let's review the check info
+# Let's review the check info for Sa
 Get-DbcCheck -Tag SaDisabled | Format-List
 
 # Let's get the current config settings
-Get-DbcConfig -Name (Get-DbcCheck -Tag SaDisabled | Select-Object -Expand Config).Split(' ')
+Get-DbcConfig -Name (Get-DbcCheck -Tag SaDisabled).Config.Split(' ')
 
-<#
-Name                     Value  Description
-----                     -----  -----------
-app.sqlinstance                 List of SQL Server instances that SQL-based tests will run against
-skip.security.sadisabled True   Skip the check for if the sa login is disabled
-#>
-
-# The SaDisabled check is set to be skipped let's chance that
+# The SaDisabled check is set to be skipped let's change that
 Set-DbcConfig -Name skip.security.sadisabled -Value $false
 
 # Let's get the current config settings
-Get-DbcConfig -Name (Get-DbcCheck -Tag SaDisabled | Select-Object -Expand Config).Split(' ')
+Get-DbcConfig -Name (Get-DbcCheck -Tag SaDisabled).Config.Split(' ')
 
 #Rerun those checks!
 Invoke-DbcCheck -SqlInstance $SQLInstances -Check AutoClose, SaDisabled
 
-# Can also store these results in a database for reporting!
-New-DbaDatabase -SqlInstance $dbatools1 -Name dbachecks
+# You can also look at the CIS work that Tracey Boggiano has done for
+# adding CIS checks to dbachecks
 
-# This breaks in the container... 
-Invoke-DbcCheck -SqlInstance $SQLInstances -Check AutoClose, SaDisabled -PassThru |
-Convert-DbcResult -Label 'BitsTesting' |
-Write-DbcTable -SqlInstance $dbatools1 -Database dbachecks
+Get-DbcCheck -Tag CIS
 
-# Show the reporting...
+Get-DbcConfig -Name (Get-DbcCheck -Tag CIS).Config.Split(' ') | Where Name -ne 'app.sqlinstance'
+
+# there are a lot of Skip trues there!!
+
+Invoke-DbcCheck -SqlInstance $dbatools1 -Tag CIS
+
+# So Tracey wrote a function to help
+
+Set-DbcCisConfig 
+
+# and now we can do
+
+$CisChecks = Invoke-DbcCheck -SqlInstance $dbatools1 -Tag CIS -PassThru
+
+# Rob likes best to store these in a database, so whilst you can use Start-DbcPowerBi 
+# to open a PowerBi File which will use the natively stored json results that we do behind the scene
+
+# I like to do this
+# Convert-DbcResult tkaes the Pester results and does a little parsing of the data to set the computername
+# Sql Instance Name and database name and allows you to add a label.
+# Now you can have a historical record of your checks :-)
+# Write-DbcTable will do two things
+# Create a lookup table with the latest checks and create two tables dbo.CheckResults and dbo.dbachecksChecks
+
+$CisChecks | Convert-DbcResult -Label CIS | Write-DbcTable -SqlInstance $dbatools1 -SqlCredential $continercredential  -Database Validation 
+
+Get-DbaDbTable -SqlInstance $dbatools1 -Database Validation | Format-Table
+
+# Show the reporting database in ADS
+
+# Show the PowerBi - needs a seperate session because we are inside the container here!.
+# We need to add the hostname localhost,7433
+# user sqladmin
+# password dbatools.IO
 
 # Peek behind the scenes of these demos...
+
+Get-GameTimeRemaining
+
+# What else would you like to learn ?
